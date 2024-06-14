c7d3decca838ec777be1870c94635d5656293197
Ravindu Hasanka <rhatu2000@gmail.com>
2024-06-15 04:52:32 +0530

    initial structure'

---
+++ src/main/java/org/lafresca/lafrescabackend/LaFrescaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.lafresca.lafrescabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaFrescaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaFrescaBackendApplication.class, args);
+	}
+
+}
