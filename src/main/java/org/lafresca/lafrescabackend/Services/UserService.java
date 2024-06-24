a7b9fd3e224f7369f4f871e1640942b6c8e5ed34
Ravindu Hasanka <rhatu2000@gmail.com>
2024-06-24 19:31:00 +0530

    user delete validations

--- src/main/java/org/lafresca/lafrescabackend/Services/UserService.java
+++ src/main/java/org/lafresca/lafrescabackend/Services/UserService.java
@@ -22,6 +22,8 @@ public class UserService {
 
     public List<User> getUsers() {
         System.out.println("Getting all users");
+        List<User> users = userRepository.findAll();
+
         return userRepository.findAll();
     }
 
@@ -65,8 +67,6 @@ public class UserService {
 //        if(!cafe.isPresent()) {
 //            throw new IllegalStateException("Cafe with id " + user.getCafeId() + " does not exist");
 //        }
-
-
         userRepository.save(user);
     }
 
@@ -79,10 +79,32 @@ public class UserService {
     public void updateUser(User user) {
         User userToUpdate = userRepository.findById(user.getUserId())
                 .orElseThrow(() -> new IllegalStateException("User with id " + user.getUserId() + " does not exist"));
+
         if (user.getFirstName() != null && user.getFirstName().length() > 0 && !userToUpdate.getFirstName().equals(user.getFirstName())) {
             userToUpdate.setFirstName(user.getFirstName());
-
         }
+        if (user.getLastName() != null && user.getLastName().length() > 0 && !userToUpdate.getLastName().equals(user.getLastName())) {
+            userToUpdate.setLastName(user.getLastName());
+        }
+        if (user.getEmail() != null && user.getEmail().length() > 0 && !userToUpdate.getEmail().equals(user.getEmail()) && isValidEmail(user.getEmail())) {
+            userToUpdate.setEmail(user.getEmail());
+        }
+        if (user.getPhoneNumber() != null && user.getPhoneNumber().length() > 0 && !userToUpdate.getPhoneNumber().equals(user.getPhoneNumber()) && user.getPhoneNumber().length() == 10){
+            userToUpdate.setPhoneNumber(user.getPhoneNumber());
+        }
+        if (user.getRole() != null && user.getRole().length() > 0 && !userToUpdate.getRole().equals(user.getRole()) && ((user.getRole()=="ADMIN" || user.getRole()=="CUSOTMER" || user.getRole()=="TOP_LEVEL_MANAGER" || user.getRole()=="CAFE_MANAGER" || user.getRole()=="CASHIER" || user.getRole()=="KITCHEN_MANAGER" || user.getRole()=="WAITER" || user.getRole()=="DELIVERY_PERSON" || user.getRole()=="STOCKKEEPER"))) {
+            userToUpdate.setRole(user.getRole());
+        }
+        if (user.getPassword() != null && user.getPassword().length() > 0 && !userToUpdate.getPassword().equals(user.getPassword())) {
+            userToUpdate.setPassword(user.getPassword());
+        }
+        if (user.getAddress() != null && user.getAddress().length() > 0 && !userToUpdate.getAddress().equals(user.getAddress())) {
+            userToUpdate.setAddress(user.getAddress());
+        }
+//        Optional<Cafe> cafe = cafeRepository.findById(user.getCafeId());
+//        if (user.getCafeId() != null && user.getCafeId() != 0 && !userToUpdate.getCafeId().equals(user.getCafeId()) && cafe.isPresent()) {
+//            userToUpdate.setCafeId(user.getCafeId());
+//        }
         userRepository.save(userToUpdate);
     }
 }
