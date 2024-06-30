cc04f86d940b4520d8d7cee5b0c0287965b04964
DasunThathsara <dasun.thathsara.sri@gmail.com>
2024-06-30 21:49:44 +0530

    refactor: Food to FoodItem

--- src/main/java/org/lafresca/lafrescabackend/Repositories/FoodItemRepository.java
+++ src/main/java/org/lafresca/lafrescabackend/Repositories/FoodItemRepository.java
@@ -1,9 +1,9 @@
 package org.lafresca.lafrescabackend.Repositories;
 
-import org.lafresca.lafrescabackend.Models.Food;
+import org.lafresca.lafrescabackend.Models.FoodItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FoodRepository extends MongoRepository<Food,String> {
+public interface FoodItemRepository extends MongoRepository<FoodItem,String> {
 }
