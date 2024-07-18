0abecae81aa1b09545ead049a2d3a854566a4991
DasunThathsara <dasun.thathsara.sri@gmail.com>
2024-07-18 08:26:43 +0530

    fix: retrieve all by deleted:0

--- src/main/java/org/lafresca/lafrescabackend/Repositories/FoodItemRepository.java
+++ src/main/java/org/lafresca/lafrescabackend/Repositories/FoodItemRepository.java
@@ -2,8 +2,13 @@ package org.lafresca.lafrescabackend.Repositories;
 
 import org.lafresca.lafrescabackend.Models.FoodItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface FoodItemRepository extends MongoRepository<FoodItem,String> {
+public interface FoodItemRepository extends MongoRepository<FoodItem, String> {
+    @Query("{ 'Deleted' : ?0 }")
+    List<FoodItem> findByDeleted(Integer Deleted);
 }
