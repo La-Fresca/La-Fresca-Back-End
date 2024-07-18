ed6fa53602d0f305148169eec77daf5da9696d93
S Manivannan <suruthi0611@gmail.com>
2024-07-18 11:30:27 +0530

    feat: Initialized Cart Controller, Service and Repository

--- src/main/java/org/lafresca/lafrescabackend/Controllers/CartController.java
+++ src/main/java/org/lafresca/lafrescabackend/Controllers/CartController.java
@@ -1,4 +1,19 @@
 package org.lafresca.lafrescabackend.Controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.lafresca.lafrescabackend.Services.CartService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "/api/lafresca/cart")
+@AllArgsConstructor
+@Tag(name = "Cart Controller")
 public class CartController {
+    private final CartService cartService;
+
+
 }
--- src/main/java/org/lafresca/lafrescabackend/Services/CartService.java
+++ src/main/java/org/lafresca/lafrescabackend/Services/CartService.java
@@ -1,5 +1,15 @@
 package org.lafresca.lafrescabackend.Services;
 
+import org.lafresca.lafrescabackend.Repositories.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CartService {
+    private final CartRepository cartRepository;
+
+    @Autowired
+    public CartService(CartRepository cartRepository) { this.cartRepository = cartRepository; }
+
+
 }
