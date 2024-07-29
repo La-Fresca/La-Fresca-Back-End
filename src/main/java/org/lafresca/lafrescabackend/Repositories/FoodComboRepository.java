57946e91d66c2c1f75e0ec389d5637456bdab10a
S Manivannan <suruthi0611@gmail.com>
2024-07-30 01:49:49 +0530

    fix: Food Combo, Stock, Stock Collection Get All Method

--- src/main/java/org/lafresca/lafrescabackend/Repositories/FoodComboRepository.java
+++ src/main/java/org/lafresca/lafrescabackend/Repositories/FoodComboRepository.java
@@ -1,9 +1,15 @@
 package org.lafresca.lafrescabackend.Repositories;
 
 import org.lafresca.lafrescabackend.Models.FoodCombo;
+import org.lafresca.lafrescabackend.Models.FoodItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FoodComboRepository extends MongoRepository<FoodCombo,String> {
+    @Query("{ 'Deleted' : ?0 }")
+    List<FoodCombo> findByDeleted(Integer Deleted);
 }
