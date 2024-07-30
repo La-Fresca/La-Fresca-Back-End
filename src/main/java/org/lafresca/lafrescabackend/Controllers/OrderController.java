a3b3201f4013e77c93f43c5d9b9e9cc052995ecf
Ravindu Hasanka <rhatu2000@gmail.com>
2024-07-30 19:53:39 +0530

    Orders update

--- src/main/java/org/lafresca/lafrescabackend/Controllers/OrderController.java
+++ src/main/java/org/lafresca/lafrescabackend/Controllers/OrderController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "api/lafresca/order")
 @Tag(name = "Order Controller")
 public class OrderController {
