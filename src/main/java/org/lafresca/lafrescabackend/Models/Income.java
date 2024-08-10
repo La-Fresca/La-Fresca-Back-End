719eb6b97ab11653ec6acd678099b763eee7c3c4
S Manivannan <suruthi0611@gmail.com>
2024-08-11 03:24:48 +0530

    fix: Branch

---
+++ src/main/java/org/lafresca/lafrescabackend/Models/Income.java
@@ -0,0 +1,11 @@
+package org.lafresca.lafrescabackend.Models;
+
+import lombok.Data;
+
+@Data
+public class Income {
+    private Integer Year;
+    private Integer Month;
+    private Double Amount;
+}
+
