c42259021dce81db899fc699a09349e81b7e212b
DasunThathsara <dasun.thathsara.sri@gmail.com>
2024-09-07 15:02:55 +0530

    feat: Category DTO

--- src/main/java/org/lafresca/lafrescabackend/Services/CategoryService.java
+++ src/main/java/org/lafresca/lafrescabackend/Services/CategoryService.java
@@ -1,5 +1,8 @@
 package org.lafresca.lafrescabackend.Services;
 
+import org.lafresca.lafrescabackend.DTO.CategoryDTO;
+
+import org.lafresca.lafrescabackend.DTO.CategoryDTOMapper;
 import org.lafresca.lafrescabackend.Exceptions.ResourceNotFoundException;
 import org.lafresca.lafrescabackend.Models.Category;
 import org.lafresca.lafrescabackend.Repositories.CategoryRepository;
@@ -8,14 +11,19 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final CategoryDTOMapper categoryDTOMapper;
 
     @Autowired
-    public CategoryService(CategoryRepository categoryRepository) { this.categoryRepository = categoryRepository; }
+    public CategoryService(CategoryRepository categoryRepository, CategoryDTOMapper categoryDTOMapper) {
+        this.categoryRepository = categoryRepository;
+        this.categoryDTOMapper = categoryDTOMapper;
+    }
 
     // Add New Category
     public String addNewCategory(Category category) {
@@ -31,7 +39,7 @@ public class CategoryService {
             error = "Cafe id cannot be empty";
         }
 
-        Category alreadyExisting = categoryRepository.findByName(category.getName());
+        Category alreadyExisting = categoryRepository.findByName(category.getCafeId(), category.getName());
 
         if (alreadyExisting != null) {
             error = "Category already exists";
@@ -45,7 +53,12 @@ public class CategoryService {
     }
 
     // Get All Categories
-    public List<Category> getCategories() { return categoryRepository.findAll(); }
+    public List<CategoryDTO> getCategories(String cafeId) {
+        return categoryRepository.findByCafeId(cafeId)
+                .stream()
+                .map(categoryDTOMapper)
+                .collect(Collectors.toList());
+    }
 
     // Search Category
     public Optional<Category> getCategory(String id) {
