66ff10cdb65a104ebed596ee4c8ceaa7bb5a22f3
S Manivannan <suruthi0611@gmail.com>
2024-11-24 15:33:26 +0530

    feat: Send OTP, Monthly Statement, Online Order Invoice and Ongoing Discount Information Functionalities

--- src/main/java/org/lafresca/lafrescabackend/Services/EmailService.java
+++ src/main/java/org/lafresca/lafrescabackend/Services/EmailService.java
@@ -21,12 +21,127 @@ public class EmailService {
         User user = userRepository.findById(userId)
                 .orElseThrow(()-> new ResourceNotFoundException("User not found with id " + userId));
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo("suruthi0611@gmail.com");
+        message.setTo(user.getEmail());
         message.setSubject("Your One-Time Password");
         message.setText("<div>"
-                + "Hello " + user.getFirstName() + " " + user.getLastName() + ","
-                + "Your one time password is: " + OTP
-                + "Lafresca"
+                + "Hello " + user.getFirstName() + " " + user.getLastName() + ",\n"
+                + "</div>"
+                + "<div>"
+                + "Thank you for registering with us! To complete your registration and verify your email address, please use the following One-Time Password (OTP)\n"
+                + "Your OTP: " + OTP
+                + "</div>"
+                + "<div>"
+                + "\nThis OTP is valid for the next 5 minutes and can be used only once. Please enter this OTP in the designated field to proceed with the verification process.\n"
+                + "</div>"
+                + "<div>"
+                + "Thank you for choosing La Fresca. We look forward to welcoming you to our community of coffee lovers and food enthusiasts!\n"
+                + "La Fresca Team"
+                + "</div>");
+        message.setFrom("thecafe.lafresca@gmail.com");
+
+        javaMailSender.send(message);
+        return "Email sent successfully";
+    }
+
+    public String sendMonthlyStatement(String userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new ResourceNotFoundException("User not found with id " + userId));
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getEmail());
+        message.setSubject("Monthly Income Statement Overview - [Month, Year]");
+        message.setText("<div>"
+                + "Dear " + user.getFirstName() + " " + user.getLastName() + ",\n"
+                + "</div>"
+                + "<div>"
+                + "This automated notification serves to inform you that the monthly income statement for La Fresca's [Branch Name] Branch for [Month, Year] is now available. The attached document provides a detailed overview of our financial performance including revenues, expenses, and net income.\n"
+                + "</div>"
+                + "We kindly request that you review the attached income statement to assess our financial standing and to aid in strategic decision-making for the coming months.\n"
+                + "</div>"
+                + "<div>"
+                + "Should you encounter any discrepancies or require further details, please contact our finance department directly at [Finance Department Contact Information].\n"
+                + "</div>"
+                + "<div>"
+                + "We appreciate your oversight and guidance.\n"
+                + "Warm regards,\n"
+                + "La Fresca Finance Team"
+                + "</div>");
+        message.setFrom("thecafe.lafresca@gmail.com");
+
+        javaMailSender.send(message);
+        return "Email sent successfully";
+    }
+
+    public String sendInvoice(String userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new ResourceNotFoundException("User not found with id " + userId));
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getEmail());
+        message.setSubject("Your La Fresca Order Receipt - Order #[Order Number]\n");
+        message.setText("<div>"
+                + "Dear " + user.getFirstName() + " " + user.getLastName() + ",\n"
+                + "</div>"
+                + "<div>"
+                + "Thank you for choosing La Fresca! This email confirms your recent order placed on [Order Date]. Below, you'll find a detailed receipt of your purchase.\n"
+                + "</div>"
+                + "<div>"
+                + "Order Summary:\n"
+                + "</div>"
+                + "<div>"
+                + "Order Number: [Order Number]\n" +
+                "Date: [Order Date]\n" +
+                "Total Amount: [Total Amount]\n" +
+                "Payment Method: [Payment Method]\n"
+                + "</div>"
+                + "<div>"
+                + "Items Ordered:\n"
+                + "</div>"
+                + "<div>"
+                + "Item 1 - Quantity: X - Price: $Y\n" +
+                "Item 2 - Quantity: X - Price: $Y ... (additional items as necessary)\n"
+                + "</div>"
+                + "<div>"
+                + "Delivery Details:\n"
+                + "</div>"
+                + "<div>"
+                + "Delivery Address: [Customer’s Address]\n" +
+                "Estimated Delivery Time: [Delivery Time]\n"
+                + "</div>"
+                + "<div>"
+                + "We hope you enjoy your meal and look forward to serving you again soon.\n"
+                + "</div>"
+                + "<div>"
+                + "Contact us at [Contact Information] for any inquiries.\n"
+                + "</div>");
+        message.setFrom("thecafe.lafresca@gmail.com");
+
+        javaMailSender.send(message);
+        return "Email sent successfully";
+    }
+
+    public String sendOngoingDiscount(String userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new ResourceNotFoundException("User not found with id " + userId));
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getEmail());
+        message.setSubject("Exclusive Discounts Just for You at La Fresca!");
+        message.setText("<div>"
+                + "Dear " + user.getFirstName() + " " + user.getLastName() + ",\n"
+                + "</div>"
+                + "<div>"
+                + "We’re excited to share some fantastic news! As a valued customer of La Fresca, you are eligible for exclusive discounts that make enjoying your favorite meals and drinks even more delightful.\n"
+                + "</div>"
+                + "<div>"
+                + "Current Offers:\n"
+                + "</div>"
+                + "<div>"
+                + "[Discount Description 1]: Save [Percentage]% on [Specific Items or Total Order].\n" +
+                "[Discount Description 2]: Buy one [Specific Items] and get one [Specific Items] Free!!\n"
+                + "</div>"
+                + "<div>"
+                + "Don’t miss out—these discounts are available for a limited time only!\n"
+                + "</div>"
+                + "<div>"
+                + "Enjoy your savings and your next meal at La Fresca!\n"
                 + "</div>");
         message.setFrom("thecafe.lafresca@gmail.com");
 
