e27a6c9d3bef2adb138bfa3c19c25b02596fa28c
Ravindu Hasanka <98080235+Ravindu-Hasanka@users.noreply.github.com>
2024-11-26 14:28:25 +0530

    sales prediction service webclient created

--- src/main/java/org/lafresca/lafrescabackend/Orchestrator/SalesPredictionService.java
+++ src/main/java/org/lafresca/lafrescabackend/Orchestrator/SalesPredictionService.java
@@ -1,8 +1,24 @@
 package org.lafresca.lafrescabackend.Orchestrator;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class SalesPredictionService {
+    private final WebClient webClient;
+    private final String url = "http://external-api-url.com";
 
+    public SalesPredictionService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl(url).build(); // Set the base URL
+    }
+
+    public Map<String, Map<String, Integer>> getBranchData() {
+        return webClient.get()
+                .uri("/endpoint")
+                .retrieve()
+                .bodyToMono(Map.class) // Specify the type as Map.class
+                .block(); // Block to get the result directly
+    }
 }
--- src/main/java/org/lafresca/lafrescabackend/Orchestrator/ServiceOrchestrator.java
+++ src/main/java/org/lafresca/lafrescabackend/Orchestrator/ServiceOrchestrator.java
@@ -2,13 +2,19 @@ package org.lafresca.lafrescabackend.Orchestrator;
 
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class ServiceOrchestrator {
     private final SalesPredictionService salesPredictionService;
     private final StockPredictionService stockPredictionService;
 
+    private Map<String, Map<String, String>> branches;
+
     public ServiceOrchestrator(SalesPredictionService salesPredictionService, StockPredictionService stockPredictionService) {
         this.salesPredictionService = salesPredictionService;
         this.stockPredictionService = stockPredictionService;
     }
+
+
 }
