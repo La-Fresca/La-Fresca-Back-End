06329664b0f5e96df8538f598c0e5c8b78adaff2
Ravindu Hasanka <98080235+Ravindu-Hasanka@users.noreply.github.com>
2024-11-30 23:37:10 +0530

    Branch Service logs added

--- src/main/java/org/lafresca/lafrescabackend/Services/BranchService.java
+++ src/main/java/org/lafresca/lafrescabackend/Services/BranchService.java
@@ -8,10 +8,12 @@ import org.lafresca.lafrescabackend.Models.*;
 import org.lafresca.lafrescabackend.Repositories.BranchRepository;
 import org.lafresca.lafrescabackend.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -22,10 +24,12 @@ import java.util.Optional;
 public class BranchService {
     private final BranchRepository branchRepository;
     private final UserRepository userRepository;
+    private final SystemLogService systemLogService;
 
     @Autowired
-    public BranchService(BranchRepository branchRepository, UserRepository userRepository) { this.branchRepository = branchRepository;
+    public BranchService(BranchRepository branchRepository, UserRepository userRepository, SystemLogService systemLogService) { this.branchRepository = branchRepository;
         this.userRepository = userRepository;
+        this.systemLogService = systemLogService;
     }
 
     // Add new branch
@@ -46,17 +50,35 @@ public class BranchService {
         branchManager.setCafeId(savedBranch.getId());
         userRepository.save(branchManager);
 
+        String user = SecurityContextHolder.getContext().getAuthentication().getName();
+        LocalDateTime now = LocalDateTime.now();
+
+        String message = now + " " + user + " " + "Created New Branch - " + savedBranch.getId();
+        systemLogService.writeToFile(message);
         return branch;
     }
 
     // Retrieve all branches
     public List<Branch> getBranches() {
+        String user = SecurityContextHolder.getContext().getAuthentication().getName();
+        LocalDateTime now = LocalDateTime.now();
+
+        String message = now + " " + user + " " + "Retrieve all branches" ;
+        systemLogService.writeToFile(message);
+
         return branchRepository.findByDeleted();
     }
 
     // Search branch by id
     public Optional<Branch> getBranch(String id) {
         branchRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Branch not found with id " + id));
+
+        String user = SecurityContextHolder.getContext().getAuthentication().getName();
+        LocalDateTime now = LocalDateTime.now();
+
+        String message = now + " " + user + " " + "Retrieve branche by Id - " + id;
+        systemLogService.writeToFile(message);
+
         return branchRepository.findById(id);
     }
 
@@ -64,6 +86,12 @@ public class BranchService {
     public void deleteBranch(String id) {
         branchRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Branch not found with id " + id));
         branchRepository.deleteById(id);
+
+        String user = SecurityContextHolder.getContext().getAuthentication().getName();
+        LocalDateTime now = LocalDateTime.now();
+
+        String message = now + " " + user + " " + "Delete branch - " + id ;
+        systemLogService.writeToFile(message);
     }
 
     // Update branch by id
@@ -87,6 +115,12 @@ public class BranchService {
         }
 
         branchRepository.save(existingBranch);
+
+        String user = SecurityContextHolder.getContext().getAuthentication().getName();
+        LocalDateTime now = LocalDateTime.now();
+
+        String message = now + " " + user + " " + "Updated branch " + id ;
+        systemLogService.writeToFile(message);
     }
 
     // Logical Delete
@@ -96,14 +130,13 @@ public class BranchService {
 
         existingBranch.setDeleted(1);
 
+        String user = SecurityContextHolder.getContext().getAuthentication().getName();
+        LocalDateTime now = LocalDateTime.now();
+
+        String message = now + " " + user + " " + "Deleted branch logically - " + id ;
+        systemLogService.writeToFile(message);
+
         branchRepository.save(existingBranch);
     }
 
-
-
-    //branch statistics
-//    public BranchStat getBranchStat(String id) {
-//        Date today  = new Date();
-//
-//    }
 }
